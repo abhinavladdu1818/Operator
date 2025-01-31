e804661da0e6843f3cea9fbdf300b43017936fe3
abhinavladdu1818 <laddumudhiraj1818@gmail.com>
2025-01-31 10:28:17 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,10 @@
+public class Operator {
+        public static void main(String[]args){
+        int a = 12;
+        int b = 20;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);}
+    }
+
